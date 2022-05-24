370ba76c5e0cef3e7d90339f703120819885d725
kehaoyuan <kehaoyuan@ghzhushou.com>
2022-05-24 14:11:50 +0800

    fix: fix build exception feat: remove v7 library(修复由于v7与v4版本不一致导致的闪退问题)

--- library/src/main/java/com/bumptech/glide/load/resource/drawable/DrawableDecoderCompat.java
+++ library/src/main/java/com/bumptech/glide/load/resource/drawable/DrawableDecoderCompat.java
@@ -8,15 +8,12 @@ import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
-import android.support.v7.content.res.AppCompatResources;
-import android.support.v7.view.ContextThemeWrapper;
 
 /**
  * Handles decoding Drawables with the v7 support library if present and falling back to the v4
  * support library otherwise.
  */
 public final class DrawableDecoderCompat {
-  private static volatile boolean shouldCallAppCompatResources = true;
   private DrawableDecoderCompat() {
     // Utility class.
   }
@@ -30,11 +27,8 @@ public final class DrawableDecoderCompat {
   }
 
   /**
-   * Loads a Drawable using {@link AppCompatResources} if available and {@link ResourcesCompat}
-   * otherwise, depending on whether or not the v7 support library is included in the application.
-   *
    * @param theme Used instead of the {@link Theme} returned from the given {@link Context} if
-   * non-null when loading the {@link Drawable}.
+   *              non-null when loading the {@link Drawable}.
    */
   public static Drawable getDrawable(
       Context ourContext, @DrawableRes int id, @Nullable Theme theme) {
@@ -44,17 +38,6 @@ public final class DrawableDecoderCompat {
   private static Drawable getDrawable(
       Context ourContext, Context targetContext, @DrawableRes int id, @Nullable Theme theme) {
     try {
-      // Race conditions may cause us to attempt to load using v7 more than once. That's ok since
-      // this check is a modest optimization and the output will be correct anyway.
-      if (shouldCallAppCompatResources) {
-        return loadDrawableV7(targetContext, id, theme);
-      }
-    } catch (NoClassDefFoundError error) {
-      shouldCallAppCompatResources = false;
-    } catch (IllegalStateException e) {
-      if (ourContext.getPackageName().equals(targetContext.getPackageName())) {
-        throw e;
-      }
       return ContextCompat.getDrawable(targetContext, id);
     } catch (Resources.NotFoundException e) {
       // Ignored, this can be thrown when drawable compat attempts to decode a canary resource. If
@@ -62,12 +45,6 @@ public final class DrawableDecoderCompat {
     }
 
     return loadDrawableV4(targetContext, id, theme != null ? theme : targetContext.getTheme());
-  }
-
-  private static Drawable loadDrawableV7(Context context, @DrawableRes int id,
-      @Nullable Theme theme) {
-    Context resourceContext = theme != null ? new ContextThemeWrapper(context, theme) : context;
-    return AppCompatResources.getDrawable(resourceContext, id);
   }
 
   private static Drawable loadDrawableV4(
